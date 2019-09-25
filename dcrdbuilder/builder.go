4a23d640cdf5124d7247f2554f05774542bf200d
J Fixby <github@jfixby.com>
2019-09-25 11:37:40 +0200

    update imports

--- dcrdbuilder/builder.go
+++ dcrdbuilder/builder.go
@@ -33,7 +33,7 @@ func nameGenerator(data string) string {
 }
 
 func fileGenerator(data string) string {
-	data = coinknife.Replace(data, `github.com\decred\dcrd`, `github.com\picfight\dcrd`)
+	data = coinknife.Replace(data, `github.com/decred/dcrd`, `github.com/picfight/dcrd`)
 	return data
 }
 
